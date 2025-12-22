6a9d6b8833083928048ac3282f4f96df4149adf2
joramirez <alejandro.ramirez@citi.com.mx>
2025-12-22 10:01:13 -0600

    Se agrega paquete de prueba

---
+++ CoreKit/PublicMethod/testMethod.swift
@@ -0,0 +1,14 @@
+//
+//  testMethod.swift
+//  CoreKit
+//
+//  Created by Jose Alejandro Ramirez Vergara on 22/12/25.
+//
+
+public final class Test {
+    public init() {}
+    
+    public func testPrint() {
+        print("Hello World")
+    }
+}
