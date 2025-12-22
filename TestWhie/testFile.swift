cb64aa5434b4940adc2efd8257a4cd1346c8014e
joramirez <alejandro.ramirez@citi.com.mx>
2025-12-22 10:25:24 -0600

    Se agrega test

---
+++ TestWhie/testFile.swift
@@ -0,0 +1,11 @@
+//
+//  testFile.swift
+//  TestCoreWhite
+//
+//  Created by Jose Alejandro Ramirez Vergara on 22/12/25.
+//
+
+public final class Test {
+    public init() {}
+    public func testPrint() { print("Hello World") }
+}
